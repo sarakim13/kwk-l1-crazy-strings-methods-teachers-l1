6dbceca6ebb0ce96a856e72c057eb2361563710c
Sara Kim <sarakim0903@gmail.com>
2018-07-24 18:31:26 +0000

    automatically backed up by learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
+ 
 # Crazy Strings
 
 That's Nonsense!
---
+++ time.rb
@@ -0,0 +1,10 @@
+time = 23
+if time >= 7 && time <= 11
+  puts "Good Morning"
+elsif time >= 11 && time <= 17
+  puts "Good Afternoon"
+elsif time >= 17 && time <= 20 
+  puts "Good Evening"
+else
+  puts "good night"
+end
